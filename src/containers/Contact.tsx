84363ce48add41370723973204184df7b3cc888c
Alex Hughes <alex@alexjackhughes.com>
2020-02-25 00:37:16 +0000

    Added a contact page

--- src/containers/Contact.tsx
+++ src/containers/Contact.tsx
@@ -1,8 +1,55 @@
 import React from "react";
+import Navigation from "../components/Navigation";
+import Layout from "../components/Layout";
 
-// TODO: Create a form page
-export default () => (
-  <div>
-    <h1>hello</h1>
-  </div>
+import { ReactComponent as TopCurve } from "../images/curves/primary.svg";
+
+const Contact: React.FC = () => (
+  <>
+    <div className="hero is-medium is-fullheight">
+      <Navigation />
+      <div className="hero-body">
+        <Layout>
+          <h1 className="title is-1" id="hero-title">
+            Let's talk
+          </h1>
+          <h3 className="subtitle">
+            If you ever want just want to ask me a question, or get in touch -
+            send me an email below. I'll usually respond instantly because I'm
+            sad like that.
+          </h3>
+          <form>
+            <div className="field">
+              <p className="control has-icons-left">
+                <input
+                  className="input is-primary"
+                  type="email"
+                  placeholder="johnny@cash.com"
+                />
+                <span className="icon is-small is-left">
+                  <i className="fad fa-mailbox" />
+                </span>
+              </p>
+            </div>
+            <div className="field is-primary">
+              <p className="control ">
+                <textarea className="textarea" placeholder="Textarea" />
+              </p>
+            </div>
+            <button className="button is-rounded is-primary is-outlined">
+              <span className="icon is-medium">
+                <i className="fad fa-paper-plane" />
+              </span>
+              <span className="has-text-weight-bold">Send message</span>
+            </button>
+          </form>
+        </Layout>
+      </div>
+      <div className="hero-bottom">
+        <TopCurve className="curve is-marginless is-paddingless flip-horizontal" />
+      </div>
+    </div>
+  </>
 );
+
+export default Contact;
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.scss";
 import Home from "./containers/Home";
+import Contact from "./containers/Contact";
+
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.render(<Contact />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
